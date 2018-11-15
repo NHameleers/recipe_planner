bf6098b817e9c8f7411bae2ba091850b15d39139
NHameleers <niels.hameleers@gmail.com>
2018-11-15 22:43:56 +0100

    Added shopping list and ability to email it

--- recipe_planner/urls.py
+++ recipe_planner/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'recipe_planner'
 urlpatterns = [
-	path('', views.IndexView.as_view(), name='index'),
+	path('', views.IndexView, name='index'),
 	path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 	path('<int:pk>', views.IngredientDetailView.as_view(), name='ingredient_detail'),
 	path('ingredient_list/', views.ingredient_list, name='ingredient_list'),
--- recipe_planner/views.py
+++ recipe_planner/views.py
@@ -3,18 +3,51 @@ from django.shortcuts import get_object_or_404, HttpResponse, render
 from django.urls import reverse
 from django.views import generic
 
+from django.conf import settings
+from django.core.mail import send_mail
+
+
+
+from collections import Counter
+
 
 from .models import Recipe, Ingredient
 
 
 
-class IndexView(generic.ListView):
+def IndexView(request):
 	template_name = 'recipe_planner/index.html'
-	context_object_name = 'latest_recipe_list'
+	context = {'latest_recipe_list': Recipe.objects.order_by('last_in_menu')}
 
-	def get_queryset(self):
-		return Recipe.objects.order_by('last_in_menu')
+	# control flow for adding ingredients of selected recipes to list
+	recipes_to_add = request.POST.getlist('recipes_to_add')
+	ingredients_to_add = Ingredient.objects.filter(recipe__name__in=recipes_to_add)
+	shopping_list = Counter(ingredients_to_add)
+	context.update({'ingredients_to_add': ingredients_to_add,
+		'shopping_list': shopping_list.items()})
 
+	# control flow for sending shopping list to email
+	email_address = request.POST.get('email_address')
+
+	email_message = ''' Shopping list:\n'''
+	for ingr, amount in shopping_list.items():
+		recipes_with_ingredient = [r.name for r in Ingredient.objects.get(name=ingr).recipe_set.all() if r.name in recipes_to_add]
+		email_message += '{}x  {} ({})\n'.format(amount, ingr, ','.join(recipes_with_ingredient))
+	email_message += '\nSelected recipes:\n'
+	for rec in recipes_to_add:
+		email_message += '{}\n'.format(rec.title())
+
+	context.update({'email_message': email_message})
+	
+	send_mail(
+	    'Shopping list',
+	    email_message,
+	    settings.EMAIL_HOST_USER,
+	    [email_address],
+	    fail_silently=False,
+	)
+
+	return render(request, template_name, context)
 
 
 class DetailView(generic.DetailView):
@@ -30,18 +63,49 @@ class IngredientDetailView(generic.DetailView):
 
 
 def ingredient_list(request):
+	# list of all recipes to use for multiselect
 	all_ingredients_list = Ingredient.objects.all()
 	
-	# this works with the names instead of the ideas. Not the greatest solution,
-	# as it could run into trouble when names get duplicates. I was able to pass
-	# the id through from the template file (option --> value), but this is not
-	# working anymore.
+
+
+	# control flow for searching recipes with selected ingredients
 	ingredient_names = request.POST.getlist('ingredients')
 
 	recipes = Recipe.objects.filter(ingredients__name__in=ingredient_names).distinct().order_by('last_in_menu')
 
 	context = {'all_ingredients_list': all_ingredients_list,
 	'recipes': recipes}
+
+
+	# control flow for adding ingredients of selected recipes to list
+	recipes_to_add = request.POST.getlist('recipes_to_add')
+	ingredients_to_add = Ingredient.objects.filter(recipe__name__in=recipes_to_add)
+	shopping_list = Counter(ingredients_to_add)
+	context.update({'ingredients_to_add': ingredients_to_add,
+		'shopping_list': shopping_list.items()})
+
+
+
+	# control flow for sending shopping list to email
+	email_address = request.POST.get('email_address')
+
+	email_message = ''' Shopping list:\n'''
+	for ingr, amount in shopping_list.items():
+		recipes_with_ingredient = [r.name for r in Ingredient.objects.get(name=ingr).recipe_set.all() if r.name in recipes_to_add]
+		email_message += '{}x  {} ({})\n'.format(amount, ingr, ','.join(recipes_with_ingredient))
+	email_message += '\nSelected recipes:\n'
+	for rec in recipes_to_add:
+		email_message += '{}\n'.format(rec.title())
+
+	context.update({'email_message': email_message})
+
+	send_mail(
+	    'Shopping list',
+	    email_message,
+	    settings.EMAIL_HOST_USER,
+	    [email_address],
+	    fail_silently=False,
+	)
 
 	return render(request, 'recipe_planner/ingredient_list.html', context)
 
